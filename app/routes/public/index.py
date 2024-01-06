ec04c9cb4889ca6ab2e6f068b86d6d20a6c452da
Eelco Wesemann <6771014+sgaduuw@users.noreply.github.com>
2024-01-06 23:02:31 +0100

    Create a post on every request to / and display the results of finding all posts

--- app/routes/public/index.py
+++ app/routes/public/index.py
@@ -1,9 +1,37 @@
-from flask import render_template
+from flask import jsonify
 from app.routes.public import public
 
 from app.models import Author, Post
+from lorem import sentence, paragraph
+
+
+def create_author(nick: str):
+    author = Author.find_one(Author.nick == nick).run()
+    if not author:
+        Author(
+            nick='sgaduuw',
+            first_name='Eelco',
+            last_name='Wesemann'
+        ).save()
+    return author
+
+
+def create_ipsum_post(nick:str) -> None:
+    Post(
+        title=sentence(),
+        body=paragraph(),
+        author=Author.find_one(Author.nick == nick).run()
+    ).save()
+
 
 @public.route('/')
 def index():
-    context = {}
-    return render_template('index.j2', **context)
+    author = create_author(nick='sgaduuw')
+    create_ipsum_post(author.nick)
+    q_posts = Post.find(fetch_links=True).to_list()
+
+    posts = [{'author': q.author.nick,'title': q.title, 'body': q.body, 'publish_date': q.publish_date} for q in q_posts]
+    context = {
+        'posts': posts
+    }
+    return jsonify(context)
