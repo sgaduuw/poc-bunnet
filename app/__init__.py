0b82594c762a058994da640e83c5317d17ac4928
Eelco Wesemann <6771014+sgaduuw@users.noreply.github.com>
2024-01-06 22:54:49 +0100

    Introduce bunnet to the setup

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,29 @@
 from flask import Flask
 from app.routes.public import public
+from bunnet import init_bunnet
+from pymongo import MongoClient
+from environs import Env
+
+env = Env()
+c_mongo = MongoClient(env.str("MONGODB_URL"))
 
 class Config:
     FLASK_DEBUG = True
+
 
 def create_app() -> Flask:
     app = Flask(__name__)
     app.config.from_object(Config)
     register_extensions(app)
     register_blueprints(app)
+
+    init_bunnet(
+        database=c_mongo.poc,
+        document_models=[
+            'app.models.Author',
+            'app.models.Post'
+        ]
+    )
     return app
 
 def register_extensions(app) -> None:
